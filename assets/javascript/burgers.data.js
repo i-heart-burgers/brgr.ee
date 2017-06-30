16215a8870a3eedccc2d6c243660b7dfa7d4d2ad
Jaan Janesmae <git@janesmae.com>
2017-06-30 08:35:28 +0300

    refactoring code, separating javascript and stylesheet from index.html, separating data from functionality

---
+++ assets/javascript/burgers.data.js
@@ -0,0 +1,13 @@
+var burgers = [
+    // Tallinn, Estonia
+    [ "Boksipeatus"              , { lat : 59.4142792        , lng : 24.75707090000003  } ],
+    [ "Uulits Kadaka tn."        , { lat : 59.41197440000001 , lng : 24.668106899999998 } ],
+    [ "Uulits Soo tn."           , { lat : 59.44481880000001 , lng : 24.742375000000038 } ],
+    [ "Burger Box"               , { lat : 59.44159190000001 , lng : 24.736561199999983 } ],
+    [ "Tondi Resto"              , { lat : 59.40269670000001 , lng : 24.715362599999935 } ],
+    [ "Diner"                    , { lat : 59.38269709999999 , lng : 24.705239900000038 } ],
+    [ "Estonian Burger Factory " , { lat : 59.4288514        , lng : 24.7439291         } ],
+    [ "Toit's City"              , { lat : 59.43480760000001 , lng : 24.75978370000007  } ],
+    [ "Mack Bar-B-Que"           , { lat : 59.4296527        , lng : 24.77413439999998  } ],
+    [ "Mack Bar-B-Que"           , { lat : 59.42634          , lng : 24.723401          } ]
+];
---
+++ assets/javascript/map.base.js
@@ -0,0 +1,49 @@
+var markers = [];
+var infowindows = [];
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 59.43914046, lng: 24.75219727 },
+        zoom: 13,
+        mapTypeControl: false,
+        streetViewControl: false,
+        styles: mapstyle
+    });
+
+    for (var i = 0; i < burgers.length; i++) {
+        infowindows[i] = new google.maps.InfoWindow({
+            content : '<b>' + burgers[i][0] + '</b>'
+        });
+        markers[i] = new google.maps.Marker({
+            position  : burgers[i][1],
+            map       : map,
+            icon: {
+                path: fontawesome.markers.CUTLERY,
+                scale: 0.4,
+                strokeWeight: 0.2,
+                strokeColor: 'black',
+                strokeOpacity: 1,
+                fillColor: '#f00',
+                fillOpacity: 0.7,
+            },
+            animation : google.maps.Animation.DROP,
+            infoWindowIndex : i
+        });
+        google.maps.event.addListener(markers[i], 'mouseover',
+        function(event)
+        {
+            infowindows[this.infoWindowIndex].open(map, this);
+        });
+        google.maps.event.addListener(markers[i], 'mousedown',
+        function(event)
+        {
+            infowindows[this.infoWindowIndex].open(map, this);
+        });
+        google.maps.event.addListener(markers[i], 'mouseout',
+        function(event)
+        {
+            infowindows[this.infoWindowIndex].close(map, this);
+        });
+
+    }
+
+}
