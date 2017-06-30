1b1297ac00a0e646fd26f424bd618418c059408f
Jaan Janesmae <git@janesmae.com>
2017-06-30 13:38:09 +0300

    added diner liivalaia, lantsh and burger kitchen

--- assets/javascript/burgers.data.js
+++ assets/javascript/burgers.data.js
@@ -8,6 +8,7 @@ var burgers = [
     [ "Burger Box"                    , { lat : 59.441325 , lng : 24.736872 } ],
     [ "Tondi Resto"                   , { lat : 59.402697 , lng : 24.715363 } ],
     [ "Diner"                         , { lat : 59.382697 , lng : 24.70524  } ],
+    [ "Diner Liivalaia"               , { lat : 59.43052  , lng : 24.758824 } ],
     [ "Estonian Burger Factory "      , { lat : 59.428851 , lng : 24.743929 } ],
     [ "Toit's City"                   , { lat : 59.434749 , lng : 24.759792 } ],
     [ "Mack Bar-B-Que"                , { lat : 59.429544 , lng : 24.773965 } ],
@@ -16,9 +17,11 @@ var burgers = [
     [ "BabyBack Ribs & BBQ Pääsküla"  , { lat : 59.369084 , lng : 24.63706  } ],
     [ "Vesivärava Grill"              , { lat : 59.434155 , lng : 24.780721 } ],
     [ "Bueno Gourmet"                 , { lat : 59.438995 , lng : 24.72834  } ],
+    [ "Lantsh"                        , { lat : 59.451303 , lng : 24.717169 } ],
 
     /* Harjumaa, Estonia */
     [ "BabyBack Ribs & BBQ Viimsi"    , { lat : 59.504637 , lng : 24.827317 } ],
-    [ "Taps"                          , { lat : 59.42935  , lng : 24.551625 } ]
+    [ "Taps"                          , { lat : 59.42935  , lng : 24.551625 } ],
+    [ "Burger Kitchen"                , { lat : 59.506379 , lng : 24.830898 } ]
 
 ];
