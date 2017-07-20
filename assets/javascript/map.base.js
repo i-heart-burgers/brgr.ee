f9db2e7692af1e9d47da14ebc86ddcd184e6ee47
Jaan Janesmae <git@janesmae.com>
2017-07-20 15:35:46 +0300

    Fix: little less zoom to geolocation

--- assets/javascript/map.base.js
+++ assets/javascript/map.base.js
@@ -16,7 +16,7 @@ function initMap() {
 				lng: position.coords.longitude
 			};
 			map.setCenter(pos);
-			map.setZoom(16);
+			map.setZoom(15);
 		}, function() {
 			handleLocationError(true, '', map.getCenter());
 		});
