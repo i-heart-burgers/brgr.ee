bf2aa5e5aeaffab004faf2c49d481c14b37eed7c
Jaan Janesmae <git@janesmae.com>
2017-10-07 03:12:28 +0300

    updating some location data

--- assets/javascript/burgers.data.js
+++ assets/javascript/burgers.data.js
@@ -1,11 +1,11 @@
 var burgers = [
 
     /* Tallinn, Estonia */
-    [ "Boksipeatus"                   , { lat : 59.414279 , lng : 24.757071 } ],
-    [ "Uulits Kadaka tn."             , { lat : 59.411974 , lng : 24.668107 } ],
-    [ "Uulits Soo tn."                , { lat : 59.444944 , lng : 24.742193 } ],
-    [ "Balta Uulits"                  , { lat : 59.441012 , lng : 24.73561  } ],
-    [ "Burger Box"                    , { lat : 59.441325 , lng : 24.736872 } ],
+    [ "Boksipeatus"                   , { lat : 59.414279 , lng : 24.757071 } , "http://www.boksipeatus.ee" ,  "Veerenni 54, Tallinn" , "Mon-Fri 09.00-20.00<br>Sat 10.00-19.00<br>Sun 11.00-18.00" ],
+    [ "Uulits Kadaka tn."             , { lat : 59.411974 , lng : 24.668107 } , "https://www.uulits.ee"     , "Kadaka tee 135A, Tallinn" , "Sun-Thu 12.00-21.00<br>Fri-Sat 12.00-22.00" ],
+    [ "Uulits Soo tn."                , { lat : 59.444944 , lng : 24.742193 } , "https://www.uulits.ee"     , "Soo tn 1B/6, Tallinn" , "Sun-Thu 12.00-22.00<br>Fri-Sat 12.00-23.00" ],
+    [ "Balta Uulits"                  , { lat : 59.441012 , lng : 24.73561  } , "https://www.uulits.ee"     , "Kopli 1, Tallinn" , "Mon-Sun 10.00-20.00" ],
+    [ "Burger Box"                    , { lat : 59.441325 , lng : 24.736872 } , "", "Kopli 4a, Tallinn" , "" ],
     [ "Tondi Resto"                   , { lat : 59.402697 , lng : 24.715363 } ],
     [ "Diner"                         , { lat : 59.382697 , lng : 24.70524  } ],
     [ "Diner Liivalaia"               , { lat : 59.43052  , lng : 24.758824 } ],
@@ -17,13 +17,17 @@ var burgers = [
     [ "BabyBack Ribs & BBQ Pääsküla"  , { lat : 59.369084 , lng : 24.63706  } ],
     [ "Vesivärava Grill"              , { lat : 59.434155 , lng : 24.780721 } ],
     [ "Bueno Gourmet"                 , { lat : 59.438995 , lng : 24.72834  } ],
-    [ "Lantsh"                        , { lat : 59.451303 , lng : 24.717169 } ],
+    [ "Lantsh"                        , { lat : 59.451300 , lng : 24.717170 } ],
     [ "Räägupesa"                     , { lat : 59.414963 , lng : 24.719256 } ],
     [ "Staap"                         , { lat : 59.417494 , lng : 24.720439 } ],
     [ "Dereku Burger"                 , { lat : 59.435243 , lng : 24.769962 } ],
     [ "Päästke Willy"                 , { lat : 59.440539 , lng : 24.730536 } ],
     [ "Veska Grill Kiosk"             , { lat : 59.427551 , lng : 24.670239 } ],
     [ "Veska Grill Baar"              , { lat : 59.42779  , lng : 24.628156 } ],
+    [ "American Burger"               , { lat : 59.394692 , lng : 24.644733 } ],
+    [ "Poseidon Burgers"              , { lat : 59.451303 , lng : 24.717169 } ],
+    [ "Burgerito"                     , { lat : 59.431792 , lng : 24.745941 } ],
+    [ "R18"                           , { lat : 59.436951 , lng : 24.746013 } , "http://www.r18.ee" , "Raekoja plats 18, Tallinn", "Mon-Thu 12:00 - 20:00<br>Fri-Sat 12:00 - 21:00<br>Sun 12:00 - 20:00" ],
 
     /* Harjumaa, Estonia */
     [ "BabyBack Ribs & BBQ Viimsi"    , { lat : 59.504637 , lng : 24.827317 } ],
